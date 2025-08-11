7a5ecdf2cb5d6c756b7807743ab6391fbe58c11f
Titipon Tawong <titipon_tawong@cmu.ac.th>
2025-08-11 12:11:25 +0700

    implement navbar
    implement navbar into router and App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <div class="max-w-4xl mx-auto p-6">
-    <router-view />
+  <div class="min-h-screen bg-gray-50">
+    <NavBar />
+    <div class="max-w-4xl mx-auto p-6">
+      <router-view />
+    </div>
   </div>
-  
 </template>
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
+const NewsDetail = () => import('../pages/NewsDetail.vue')
 
 const routes = [
   { path: '/', name: 'home', component: HomePage },
+  { path: '/news/:id', name: 'news-detail', component: NewsDetail },
 ]
 
 export const router = createRouter({
