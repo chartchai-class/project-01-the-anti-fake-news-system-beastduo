3281d92031a3df75f8690fcfca599ee7bcabb283
Titipon Tawong <titipon_tawong@cmu.ac.th>
2025-08-11 12:33:13 +0700

    update router to include about, contact

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 const NewsDetail = () => import('../pages/NewsDetail.vue')
+const AboutView = () => import('../pages/AboutView.vue')
+const ContactView = () => import('../pages/ContactView.vue')
 
 const routes = [
   { path: '/', name: 'home', component: HomePage },
   { path: '/news/:id', name: 'news-detail', component: NewsDetail },
+  { path: '/about', name: 'about', component: AboutView },
+  { path: '/contact', name: 'contact', component: ContactView },
 ]
 
 export const router = createRouter({
